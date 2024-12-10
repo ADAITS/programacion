3b67110b92b2a876bf971c3d5b34378621baa378
Gabriel <gabriel.rodriguez@adaits.es>
2024-12-10 17:19:20 +0100

    ejercicios(trimestre 1): subir ejercicios de librerias

--- 2425/1T/7-funciones/Liberia.java
+++ 2425/1T/7-funciones/Liberia.java
@@ -6,15 +6,15 @@ public class Liberia {
     public static void main(String[] args) {
        //Utils.print("World");
        
-       int[] numeros = { 1,2,3,4,5,6,7,8,9 };
+       int[] numeros = { 2,3,4,6,7,8,9 };
        System.out.println(Arrays.toString(numeros));
 
+       numeros = Utils.add(1, numeros); 
        numeros = Utils.add(numeros, 10);
+       numeros = Utils.add(numeros, 5, 4);
        System.out.println(Arrays.toString(numeros));
 
-       numeros = Utils.pop(numeros);
-       numeros = Utils.pop(numeros);
-       numeros = Utils.pop(numeros);
+       numeros = Utils.pop(numeros, 4);
        System.out.println(Arrays.toString(numeros));
 
     }
